00452f59c6926321186ab8a5c65cf6a3b8000f65
Kaloian Ianev <kianev@gmail.com>
2018-05-04 14:56:29 +0300

    routes created

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 
 const app = express();
 
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose.connect(db)
+  .then(() => console.log('MongoDB conneccted'))
+  .catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello!'));
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
