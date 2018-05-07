7bce8359b6b140b04279ce4597b6d05658330ebb
Kaloian Ianev <kianev@gmail.com>
2018-05-07 11:14:34 +0300

    Post model and posts api request to server created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-//@route   GET api/posts/test
-//@desc    Tests posts route
-//@access  public
+//Post model
+const Post = require('../../models/Post');
+
+//Post vaidator
+const validatePostInput = require('../../validation/post');
+
+//@route   GET api/posts/test @desc    Tests posts route @access  public
 router.get('/test', (req, res) => res.json({msg: 'posts works'}));
+
+//@route   POST api/posts  @desc Create post route @access  private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const {errors, isValid} = validatePostInput(req.body);
+
+  if (!isValid) {
+    return res
+      .status(400)
+      .json(errors);
+  }
+
+  const newPost = new Post({
+    text: req.body.text, 
+    name: req.body.name, 
+    avatar: req.body.avatar, 
+    user: req.user.id});
+
+  newPost
+    .save()
+    .then(post => res.json(post));
+});
 
 module.exports = router;
