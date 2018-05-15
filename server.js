af127d1be4f0236e8534496855ad3cc515193bab
Kaloian Ianev <kianev@gmail.com>
2018-05-15 11:14:30 +0300

    prepared for deploy

--- server.js
+++ server.js
@@ -33,15 +33,16 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-//Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static('/client/build'));
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+
 
 const port = process.env.PORT || 5000;
 
